9db12e0b5373df0e4327194a3e57056495aecd7b
bisuper1st <85873590+bisuper1st@users.noreply.github.com>
2021-06-14 17:33:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const CONFIG = {
     introTitle: 'Hương ơi!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao cứ đi nhầm đường
-    Lạc vào tim tui lẻ loi thế này`,
+    introDesc: `I still remember the first time i saw u and that feeling was phenomenal.`,
     btnIntro: 'hihi',
     title: 'Phải chăng Hương đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng Hương đã say cái bụng mỡ này :3 ',
@@ -10,9 +8,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :)',
     question:'Trên thế giới hơn 7 tỉ người mà sao Hương lại thích tui <3',
     btnReply: 'Gửi cho tui <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    reply: 'Thích thì thích thôi, đúng hong nè <33333333',
+    mess: 'Tui biết mà 🥰. Tui cũng thích Hương nữa 😘😘',
     messDesc: 'Tối nay đừng có ngủ sớm nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/bi03012000/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
